32e61e5da733c7f4bc983b16f9a2b7b4b9b42bd0
YevheniyIshchenko <yevgeniyischenko@gmail.com>
2021-07-16 13:03:24 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Header from "./components/header/Header.jsx";
 import Calendar from "./components/calendar/Calendar.jsx";
+import moment from "moment";
 
 import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
 
 import "./common.scss";
 
-class App extends Component {
-  state = {
-    weekStartDate: new Date(),
-  };
+const App = () => {
+  const [weekStartDate, setweekStartDate] = useState(new Date());
 
-  render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
-    return (
-      <>
-        <Header />
-        <Calendar weekDates={weekDates} />
-      </>
-    );
-  }
-}
+  const onPreviousWeek = () =>
+    setweekStartDate(new Date(moment(weekStartDate).day(-7)));
+
+  const onNextWeek = () =>
+    setweekStartDate(new Date(moment(weekStartDate).day(+7)));
+
+  const onCurrentDate = () => setweekStartDate(new Date());
+
+  return (
+    <>
+      <Header
+        onPreviousWeek={onPreviousWeek}
+        onNextWeek={onNextWeek}
+        onCurrentDate={onCurrentDate}
+      />
+      <Calendar weekDates={weekDates} />
+    </>
+  );
+};
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,21 +4,32 @@ import { months } from "../../utils/dateUtils.js";
 
 import "./header.scss";
 
-const Header = () => {
+const Header = ({ onPreviousWeek, onNextWeek, onCurrentDate }) => {
   return (
-    <header className="header">
-      <button className="button create-event-btn">
-        <i className="fas fa-plus create-event-btn__icon"></i>Create
+    <header className='header'>
+      <button className='button create-event-btn'>
+        <i className='fas fa-plus create-event-btn__icon'></i>Create
       </button>
-      <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
-          <i className="fas fa-chevron-left"></i>
+      <div className='navigation'>
+        <button
+          className='navigation__today-btn button'
+          onClick={onCurrentDate}
+        >
+          Today
         </button>
-        <button className="icon-button navigation__nav-icon">
-          <i className="fas fa-chevron-right"></i>
+        <button
+          className='icon-button navigation__nav-icon'
+          onClick={onPreviousWeek}
+        >
+          <i className='fas fa-chevron-left'></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <button
+          className='icon-button navigation__nav-icon'
+          onClick={onNextWeek}
+        >
+          <i className='fas fa-chevron-right'></i>
+        </button>
+        <span className='navigation__displayed-month'></span>
       </div>
     </header>
   );
